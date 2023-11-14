f4c424012bf103c478a9a89ef36deb051adc8f71
FlorenciaCabrera <flooorcabre@gmail.com>
2023-11-13 21:33:50 -0300

    agregue los alert y el juego

--- code.js
+++ code.js
@@ -1,20 +1,18 @@
-// Agregar un event listener cuando el contenido de la página se ha cargado
-document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el botón de enviar por su ID
-    var botonEnviar = document.getElementById("botonEnviar");
+document.addEventListener("DOMContentLoaded", function() {
+    // Aca programo que se genere un numero random dentro del rango del 1 al 10 
+    const numeroSecreto = Math.floor(Math.random() * 10) + 1;
 
-    // Agregar un event listener para el clic en el botón
-    botonEnviar.addEventListener("click", function () {
-        // Llamada a la función que realiza la acción deseada al hacer clic en Enviar
-        enviarFormulario();
-    });
+    // Aca le pido al usuario que ingrese un numero del 1 al 10
+    const intentoUsuario = prompt("¡Bienvenid@, si adivinas el número del 1 al 10 correctamente, te ganas un 30% de descuento en tu primera compra, suerte!");
+
+    // Aca genero un alert en caso de que haya adivinado el número random.
+    if (intentoUsuario !== null) {
+        if (parseInt(intentoUsuario) === numeroSecreto) {
+            alert("¡Felicidades! Adivinaste y ganaste un código de descuento para tu primera compra, hazle captura y envíala con tu consulta");
+        } else {
+            alert(`Lo siento, el número era ${numeroSecreto}. ¡Intentalo de nuevo la próxima vez!`);
+        } //Aca genero un alert que muestre un mensaje que no era ese numero y que muestre cual era.
+    } else {
+        alert("Hasta la proxima");
+    } // Aca genero un alert en caso de que haya cancelado elegir un numero.
 });
-
-// Función que se ejecuta al hacer clic en el botón Enviar
-function enviarFormulario() {
-    // Aquí puedes realizar acciones adicionales al hacer clic en el botón Enviar
-    console.log("Botón Enviar clicado");
-
-    // Puedes agregar más lógica aquí según tus necesidades
-}
-
